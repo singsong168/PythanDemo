6254aae23095945de39ea1d045ea2c95bdc0eda7
singsong168 <singsong1522@yahoo.com.tw>
2019-11-17 08:40:52 +0800

    test7_77777

--- deom22.py
+++ deom22.py
@@ -1,6 +1,6 @@
 print("please type something:")
-v1=input("your input as:")
-v2=input("your ddddinput as:")
+v1=input("your yyyxxxinput as:")
+
 print(f"your input is:{v1}, and length={len(v1)}")
-print(f"your input is:{v2}, and length={len(v2)}")
+print(f"your input is:{v1}, and length={len(v1)}")
 
